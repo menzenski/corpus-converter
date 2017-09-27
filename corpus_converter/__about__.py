fd2c23c2d311075c528959d34852792bc4dbfb8a
Matt Menzenski <matt@mobilgov.com>
2017-09-27 15:39:28 -0500

    add more setup files

---
+++ corpus_converter/__about__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+__version__ = '0.0.1'
+
+__author__ = 'Matt Menzenski'
+__author_email__ = 'matt.menzenski@gmail.com'
+__maintainer__ = __author__
+__maintainer_email__ = __author_email__
+__url__ = 'https://github.com/menzenski/corpus-converter'
+__license__ = 'MIT'
+__description__ = 'Transform an XML corpus to and from other data formats.'
--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
 """
 Transform an XML corpus to and from other data formats.
 """
-from setuptools import find_packages, setup
+import os
+import os.path
+from setuptools import setup
 
-dependencies = ['click']
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, "corpus_converter", "__about__.py")) as f:
+    exec(f.read(), about)
 
 setup(
     name='corpus_converter',
-    version='0.0.1',
-    url='https://github.com/menzenski/corpus-converter',
-    license='BSD',
-    author='Matt Menzenski',
-    author_email='matt.menzenski@gmail.com',
-    description='Transform an XML corpus to and from other data formats.',
+    version=about['__version__'],
+    url=about['__url__'],
+    license=about['__license__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    maintainer=about['__maintainer__'],
+    maintainer_email=about['__maintainer_email__'],
+    description=about['__description__'],
     long_description=__doc__,
-    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     zip_safe=False,
     platforms='any',
-    install_requires=dependencies,
     entry_points={
         'console_scripts': [
             'corpus-converter = corpus_converter.cli:main',
@@ -27,22 +33,23 @@ setup(
     classifiers=[
         # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         # 'Development Status :: 1 - Planning',
-        # 'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 2 - Pre-Alpha',
         # 'Development Status :: 3 - Alpha',
-        'Development Status :: 4 - Beta',
+        # 'Development Status :: 4 - Beta',
         # 'Development Status :: 5 - Production/Stable',
         # 'Development Status :: 6 - Mature',
         # 'Development Status :: 7 - Inactive',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX',
         'Operating System :: MacOS',
         'Operating System :: Unix',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+    ],
+    setup_requires=['pytest-runner'],
+    tests_requires=['pytest'],
 )
